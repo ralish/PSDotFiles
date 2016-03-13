c469fbc3b68efd3a9a168749572a96d8863323b7
Samuel D. Leslie <sdl@nexiom.net>
2016-03-13 21:01:49 +1100

    Implement dotfiles directory path selection by parameter or variable

--- PSDotFiles.psm1
+++ PSDotFiles.psm1
@@ -1,47 +1,100 @@
 Function Get-DotFiles {
     <#
         .SYNOPSIS
+        Enumerates the available dotfiles components
         .DESCRIPTION
-        .PARAMETER
+        .PARAMETER Path
+        Use the specified directory as the dotfiles directory instead of $DotFilesPath.
         .EXAMPLE
         .INPUTS
         .OUTPUTS
         .NOTES
         .LINK
+        https://github.com/ralish/PSDotFiles
     #>
     [CmdletBinding()]
     Param(
+        [Parameter(Position=1,Mandatory=$false)]
+            [String]$Path
     )
+
+    Get-DotFilesSettings
 }
 
 Function Install-DotFiles {
     <#
         .SYNOPSIS
+        Installs the selected dotfiles components
         .DESCRIPTION
-        .PARAMETER
+        .PARAMETER Path
+        Use the specified directory as the dotfiles directory instead of $DotFilesPath.
         .EXAMPLE
         .INPUTS
         .OUTPUTS
         .NOTES
         .LINK
+        https://github.com/ralish/PSDotFiles
     #>
     [CmdletBinding()]
     Param(
+        [Parameter(Position=1,Mandatory=$false)]
+            [String]$Path
     )
+
+    Get-DotFilesSettings
 }
 
 Function Remove-DotFiles {
     <#
         .SYNOPSIS
+        Removes the selected dotfiles components
         .DESCRIPTION
-        .PARAMETER
+        .PARAMETER Path
+        Use the specified directory as the dotfiles directory instead of $DotFilesPath.
         .EXAMPLE
         .INPUTS
         .OUTPUTS
         .NOTES
         .LINK
+        https://github.com/ralish/PSDotFiles
     #>
     [CmdletBinding()]
     Param(
+        [Parameter(Position=1,Mandatory=$false)]
+            [String]$Path
     )
+
+    Get-DotFilesSettings
+}
+
+Function Get-DotFilesSettings {
+    if ($Path) {
+        $script:DotFilesPath = Test-DotFilesPath -Path $Path
+        if (!$script:DotFilesPath) {
+            throw "The provided dotfiles path is either not a directory or it can't be accessed."
+        }
+    } elseif ($global:DotFilesPath) {
+        $script:DotFilesPath = Test-DotFilesPath -Path $global:DotFilesPath
+        if (!$script:DotFilesPath) {
+            throw "The default dotfiles path (`$DotFilesPath) is either not a directory or it can't be accessed."
+        }
+    } else {
+        throw "No dotfiles path was provided and the default dotfiles path (`$DotFilesPath) has not been configured."
+    }
+    Write-Debug "Using dotfiles directory: $script:DotFilesPath"
+}
+
+Function Test-DotFilesPath {
+    Param(
+        [Parameter(Mandatory=$true)]
+            [String]$Path
+    )
+
+    if (Test-Path -Path $Path) {
+        $PathItem = Get-Item -Path $Path
+        if ($PathItem -is [System.IO.DirectoryInfo]) {
+            return $PathItem.FullName
+        }
+    }
+    return $false
 }
