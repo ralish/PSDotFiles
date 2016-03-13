96dc81b94dbd546bd19ff74fb73c06dbcf78f600
Samuel D. Leslie <sdl@nexiom.net>
2016-03-13 14:37:02 +1100

    The start of simpler management of dotfiles on Windows?
    Commit the skeleton of the project. May it see the light of day...

---
+++ PSDotFiles.psm1
@@ -0,0 +1,47 @@
+Function Get-DotFiles {
+    <#
+        .SYNOPSIS
+        .DESCRIPTION
+        .PARAMETER
+        .EXAMPLE
+        .INPUTS
+        .OUTPUTS
+        .NOTES
+        .LINK
+    #>
+    [CmdletBinding()]
+    Param(
+    )
+}
+
+Function Install-DotFiles {
+    <#
+        .SYNOPSIS
+        .DESCRIPTION
+        .PARAMETER
+        .EXAMPLE
+        .INPUTS
+        .OUTPUTS
+        .NOTES
+        .LINK
+    #>
+    [CmdletBinding()]
+    Param(
+    )
+}
+
+Function Remove-DotFiles {
+    <#
+        .SYNOPSIS
+        .DESCRIPTION
+        .PARAMETER
+        .EXAMPLE
+        .INPUTS
+        .OUTPUTS
+        .NOTES
+        .LINK
+    #>
+    [CmdletBinding()]
+    Param(
+    )
+}
