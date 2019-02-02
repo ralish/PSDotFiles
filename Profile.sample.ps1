01288dc112abe22175d269196da8a05baba319e8
Samuel D. Leslie <sdl@nexiom.net>
2019-02-03 09:44:33 +1100

    Update sample profile settings with new options

--- Profile.sample.ps1
+++ Profile.sample.ps1
@@ -1,5 +1,11 @@
-# The path to your dotfiles directory
+# Dotfiles directory path
 $DotFilesPath = Join-Path $HOME 'dotfiles'
 
 # Enable automatic detection of components
 $DotFilesAutodetect = $true
+
+# Enable handling of intermediate destination symlinks
+$DotFilesAllowNestedSymlinks = $true
+
+# Skip validation of metadata XML files against XSD schema
+#$DotFilesSkipMetadataSchemaChecks = $true
